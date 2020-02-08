8630711ed1ee65d47c103675a283bfe698116709
paras <parasjitaliya@gmail.com>
2020-02-08 13:01:49 +0530

    Added:refactor employee attendance check

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,1 +1,14 @@
 echo "welcome to EmployeeWage Computation"
+#UC-1:Employee present or not
+random_num=$(( $RANDOM%3))
+echo $random_num
+if [ $random_num=1 ]
+then
+	echo "yes,this full-day employee is present"
+elif [ $random_num=2 ]
+then
+	echo "yes,this half-day employee is not present"
+else
+	echo "this employee is not present"
+
+fi	
