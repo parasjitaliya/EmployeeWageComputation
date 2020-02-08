7c50cc653ab85336498b5e4ffc868bdb081105da
paras <parasjitaliya@gmail.com>
2020-02-08 13:05:57 +0530

    Added:refector DailyEmployeeWage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -5,6 +5,10 @@ echo $random_num
 if [ $random_num=1 ]
 then
 	echo "yes,this full-day employee is present"
+	Wage_per_hour=20
+	Day_Hour=8
+	Daily_Wage=$((Wage_per_hour * Day_Hour))
+	echo "Daily_Wage=" $Daily_Wage
 elif [ $random_num=2 ]
 then
 	echo "yes,this half-day employee is not present"
